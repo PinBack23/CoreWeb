59773e8bac20b0778f27e67b81ad6abe3ecc417e
PinBack23 <pinback@outlook.de>
2017-11-16 09:36:38 +0100

    Initial checkin

---
+++ Readme.txt
@@ -0,0 +1,33 @@
+
+Tutorial 
+------------------------------------
+https://identityserver4.readthedocs.io/en/release/quickstarts/0_overview.html
+
+Anwendung
+------------------------------------
+Starten von Server: dotnet .\IdentityServer.dll
+Starten von Console: dotnet .\ConsoleApp.dll
+
+Links
+------------------------------------
+
+thinktecture
+------------
+https://github.com/IdentityServer/IdentityServer4.Samples
+
+IdentityServer4 + Web Api + Spa
+------------
+https://stackoverflow.com/questions/39594332/identityserver4-web-api-spa-together-in-one-project
+https://github.com/ChrisRichner/CoreWebApp.Quickstart
+
+OAUTH2 IMPLICIT FLOW WITH ANGULARJS AND ASP.NET CORE 2.0 IDENTITYSERVER4
+------------
+https://damienbod.com/2015/11/08/oauth2-implicit-flow-with-angular-and-asp-net-5-identity-server/
+https://github.com/damienbod/AspNet5IdentityServerAngularImplicitFlow#history
+E:\MyTestProjects\AspNet5IdentityServerAngularImplicitFlow-master
+
+Node.js & ASP.NET Core 1.0
+------------
+https://manuel-rauber.com/2016/03/07/node-js-asp-net-core-1-0-a-usage-comparison/
+https://github.com/thinktecture/nodejs-aspnetcore-webapi/
+E:\MyTestProjects\nodejs-aspnetcore-webapi-master
---
+++ src/IdentityServer/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using IdentityServer4.AccessTokenValidation;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
+                //.AddTestUsers(Config.GetUsers());
+
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    //options.ApiSecret = "secret";
+                    options.ApiName = "api1";
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Route Default Files (index.html) https://wipdeveloper.com/2016/01/09/asp-net-5-static-files-enable-default-files/
+            app.UseDefaultFiles();
+            //Route zu wwwroot
+            app.UseStaticFiles();
+
+            // api
+            //app.MapWhen(context => context.Request.Path.Value.StartsWith("/api"), map =>
+            //{
+            //    map.UseAuthentication();
+            //    //map.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            //    //{
+            //    //    Authority = "http://localhost:5000",
+            //    //    ScopeName = "api1",
+            //    //    ScopeSecret = "secret",
+            //    //    RequireHttpsMetadata = false,
+            //    //    SaveToken = true,
+            //    //    EnableCaching = true
+            //    //});
+
+            //    map.UseMvc();
+            //});
+
+            app.UseIdentityServer();
+
+            //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            //IdentityServerAuthenticationOptions identityServerValidationOptions = new IdentityServerAuthenticationOptions
+            //{
+            //    Authority = "http://localhost:5000",
+            //    ApiSecret = "secret",
+            //    ApiName = "api1",
+            //    SupportedTokens = SupportedTokens.Both
+            //};
+
+            //app.UseIdentityServerAuthentication(new IdentityServerAuthenticationOptions
+            //{
+            //    Authority = "http://localhost:5000",
+            //    ScopeName = "api1",
+            //    ScopeSecret = "secret",
+            //    RequireHttpsMetadata = false,
+            //    SaveToken = true,
+            //    EnableCaching = true
+            //});
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
